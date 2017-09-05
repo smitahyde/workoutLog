e161d18611243a254e21294c9ffc77cad7339499
Smita Hyde <smitahyde@gmail.com>
2017-09-05 19:18:04 -0400

    adding middleware

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use(require('./middleware/headers'))
+
 app.use('/api/test', function(req, res){
 	res.send('Hello World')
 })
 
 app.listen(3000, function(){
 	console.log("app is open on 3000!");
-});
+})
