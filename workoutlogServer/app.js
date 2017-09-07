3dd9c6c955903c7eddd92d8d6e3ddbc1cc0c05a1
Smita Hyde <smitahyde@gmail.com>
2017-09-06 22:24:13 -0400

    user .sync

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -2,68 +2,73 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-app.use(require('./middleware/headers'));
-
+app.use(require("./middleware/headers.js"))
 app.use('/api/test', function(req, res){
-	res.send('Hello World');
+	res.send("Hello World");
 });
 
-var Sequelize = require ('sequelize');
-var sequelize = new Sequelize('workoutlog','postgres', 'Meera123',{
+app.listen(3000, function(){
+	console.log('App is listening on 3000.')
+});
+
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('workoutlog', 'postgres', 'meerahyde123', {
 	host: 'localhost',
 	dialect: 'postgres'
 });
 
-sequelize.authenticate().then (
+sequelize.authenticate().then(
 	function() {
 		console.log('connected to workoutlog postgres db');
 	},
-	function (err){
+	function(err){
 		console.log(err);
 	}
 );
 
-var User = sequelize.define('user',{
+
+var User = sequelize.define('user', {
 	username: Sequelize.STRING,
 	passwordhash: Sequelize.STRING,
 });
-//creates the table in postgres
-//matches the model we defined
-//Doesn't drop the db
-User.sync();
-//User.sync({force:ture});// drops the table compeletly (line 27ish)
+
+
+//User.sync();
+User.sync({force.true})
+
+
+/*****
+*DANGER: THIS WILL DROP (DELETE) THE USER TABLE
+User.sync({ force: true }); 
+******/
+
+
+
 
 app.use(bodyParser.json());
-app.post('/api/user', function(req, res){
-	//when we post to api user, it will want a user object in the body 
-	var username = req.body.user.username;
-	var pass = req.body.user.password;  // TODO: hash this password - HASH-not human readable
-	//Need to create a user object and use sequelize to put that user into
-	// our database.
 
-	//Match the model we create above 
-	//Sequelize - take the user model and go out to the db and create this:
-	User.create({
-		username: username,
-		passwordhash: ''
-}).then(
+app.post('/api/user', function(req, res) {
+		var username = req.body.user.username;
+		var pass = req.body.user.password;
+		//Need to create a user object and use sequelize to put that user into
+		//
 
-//Sequelize is going to return the object it created from db.
-function createdSuccess(user){
-	//successful get this:
-	res.json({
-		user: user,
-		message:'create'
-	});
-},
-function createError(err){
-	res.send(500, err message);
-	}
-  );
-});
+		User.create({
+			username: username,
+			passwordhash: ""
+		}).then(
+		//Sequelize is going to return the object it created from db.
 
-
-app.listen(3000, function(){
-	console.log("app is open on 3000!");
-});	
+			function createSuccess(user){
+				res.json({
+						user: user,
+						message: 'create'
+				});
+			},
+			function createError(err){
+				res.send(500, err.message);
+			}
+		);
+	});	
 
---
+++ workoutlogServer/middleware/headers.js
@@ -0,0 +1,3 @@
+module.exports = function(req, res, next){
+	res.header("access-control-allow-origin","*")
+}
