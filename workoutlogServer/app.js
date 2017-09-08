62163b602b27bb0af34f6b221ddfc200a05bb528
Smita Hyde <smitahyde@gmail.com>
2017-09-08 13:35:09 -0400

    Jwt secret stuff

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -13,12 +14,22 @@ var User = sequelize.import(__dirname +'\\models\\user');
 
 app.use(bodyParser.json());
 app.use(require('./middleware/headers'));
+app.use(require('./middleware/validate-session'));
+//Creating a user
+
 app.use('/api/user', require('./routes/user'));
+//logging in a user
+
 app.use('/api/login',require('./routes/session'));
+//localhost:3000/api/login
+
+//app.use('/api/definition', require('./routes/definition'));
+
+
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
 
 app.listen(3000, function(){
-	console.log('App is listening on 3000.')
+	console.log('app is listening on 3000.')
 });
