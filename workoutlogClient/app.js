132d7a9977bbf00c20ae7cc301754f3cdc29f781
Smita Hyde <smitahyde@gmail.com>
2017-09-08 16:23:24 -0400

    UI Update

--- workoutlogClient/app.js
+++ workoutlogClient/app.js
@@ -1,16 +1,32 @@
 $(document).ready(function(){
 	$("#testAPI").on("click", function(){
-		console.log("It is working");
+		var test = $.ajax({
+			type: "GET",
+			url: "http://localhost:3000/api/test"
+		})
+
+		test.done(function(data){
+			console.log(data);
+		})
+
+		test.fail(function(){
+			console.log("Oh no!");
+		});
 	});
 
-var test = $.ajax({
-	type: "GET",
-	url: "http://localhost:3000/api/test"
-})
-	.done(function(data){
-		console.log(data);
-	})
-	.fail(function(){
-		console.log("Oh no!");
-	});
+	// $(testUser).click(function(){
+	// 	let user = {
+	// 		user: {
+	// 			username: 'jay',
+	// 			password: 'pass'
+	// 		}
+	// 	}
+	// 	$.ajax({
+	// 		type: "POST",
+	// 		url: "http://localhost:3000/api/user",
+	// 		data: JSON.stringify(user)
+	// 	}).done(function(data){
+	// 		console.log(data)
+	// 	})
+	// })
 });
