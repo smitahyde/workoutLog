22082635ce88a4ad8a612a4e36d0c9d347508460
Smita Hyde <smitahyde@gmail.com>
2017-09-10 19:53:00 -0400

    workout definition route

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,34 +1,33 @@
 require('dotenv').config();
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sequelize = require('./db.js');
 
-var User = sequelize.import(__dirname +'\\models\\user');
+var User = sequelize.import('./models/user');
+
+
 //Create table
-
- User.sync();
-
-// This clears the table each time the server starts
-//User.sync({ force: true })
+User.sync(); // sync( (force: true)), to drop then create eacch time the app starts!
 
 app.use(bodyParser.json());
+
+ 
+
+// This clears the table each time the server starts to test when there are bugs.
+//User.sync({ force: true })
+
+
 app.use(require('./middleware/headers'));
 app.use(require('./middleware/validate-session'));
 //Creating a user
 
 app.use('/api/user', require('./routes/user'));
-//logging in a user
-
+//login route
 app.use('/api/login',require('./routes/session'));
-//localhost:3000/api/login
+app.use('api/definition', require('.routes/definition'));
 
-//app.use('/api/definition', require('./routes/definition'));
-
-
-app.use('/api/test', function(req, res){
-	res.send("Hello World");
-});
 
 app.listen(3000, function(){
 	console.log('app is listening on 3000.')
